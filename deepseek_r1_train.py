812ec5e740335d187e614306b7737a01d650281b
lhl123-alt <1528064404@qq.com>
2025-06-18 10:59:03 +0800

    Update deepseek_r1_train.py

--- deepseek_r1_train.py
+++ deepseek_r1_train.py
@@ -50,25 +50,28 @@ def correctness_reward(prompts, completions, answer, **kwargs):
     extracted_responses = [extract_answer(r) for r in responses]
     print(f"问题:\n{prompts[0][-1]['content']}", f"\n答案:\n{answer[0]}", f"\n模型输出:\n{responses[0]}", f"\n提取后的答案:\n{extracted_responses[0]}")
     return [2.0 if response == str(ans) else 0.0 for response, ans in zip(extracted_responses, answer)]
+    
 # 生成答案是否是数字的奖励（单纯依赖结果是否正确进行奖励，条件很苛刻，会导致奖励比较稀疏，模型难以收敛，所以加上答案是否是数字的奖励，虽然答案错误，但是至少生成的是数字（对于数学问题），也要给予适当奖励）
 def digit_reward(completions, **kwargs):
     responses = [completion[0]['content'] for completion in completions]
     extracted_responses = [extract_answer(r) for r in responses]
     return [0.5 if response.isdigit() else 0.0 for response in extracted_responses]
 
-# 格式奖励
+# 格式奖励---严格
 def hard_format_reward(completions, **kwargs):
-    pattern = r"^<think>\n.*?\n</think>\n<answer>\n.*?\n</answer>\n$"
+    pattern = r"^<think>\n.*?\n</think>\n<answer>\n.*?\n</answer>\n$"   #严格按照模板
     responses = [completion[0]["content"] for completion in completions]
     matches = [re.match(pattern, response) for response in responses]
     return [0.5 if match else 0.0 for match in matches]
+    
 # 格式奖励
 def soft_format_reward(completions, **kwargs):
-    pattern = r"<think>.*?</think>\s*<answer>.*?</answer>"
+    pattern = r"<think>.*?</think>\s*<answer>.*?</answer>"    #只要有
     responses = [completion[0]["content"] for completion in completions]
     matches = [re.match(pattern, response) for response in responses]
     return [0.5 if match else 0.0 for match in matches]
-# 标记奖励（改善格式奖励稀疏问题）
+    
+# 标记奖励（改善格式奖励稀疏问题）  ---改善格式
 def mark_reward(completions, **kwargs):
     responses = [completion[0]["content"] for completion in completions]
     return [mark_num(response) for response in responses]
